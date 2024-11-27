f1b45e9a231362f4e324bf06ccc5cefc0f5b9919
longchen204 <ahazoky@gmail.com>
2024-11-28 02:59:43 +0700

    update product

--- BookGarden-Client/src/routers/routes.js
+++ BookGarden-Client/src/routers/routes.js
@@ -4,11 +4,12 @@ import Login from "../pages/Login/login";
 import PublicRoute from "../components/PublicRoute";
 import Footer from "../components/layout/Footer/footer";
 import Header from "../components/layout/Header/header";
-
+import ProductDetail from "../pages/Product/ProductDetail/productDetail";
 
 import { Layout } from "antd";
 import { withRouter } from "react-router";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductList from "../pages/Product/ProductList/productList";
 import Register from "../pages/Register/register";
 import ResetPassword from "../pages/ResetPassword/resetPassword";
 
@@ -18,11 +19,9 @@ const RouterURL = withRouter(({ location }) => {
       <Layout style={{ minHeight: "100vh" }}>
         <Layout style={{ display: "flex" }}>
           <Header />
-          <Switch>
-            <Route exact path="/home">
-              <Home />
-            </Route>
-          </Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
           <Layout>
             <Footer />
           </Layout>
@@ -30,7 +29,6 @@ const RouterURL = withRouter(({ location }) => {
       </Layout>
     </div>
   );
-
 
   const PublicContainer = () => (
     <div>
@@ -41,7 +39,7 @@ const RouterURL = withRouter(({ location }) => {
             <Route exact path="/">
               <Home />
             </Route>
-          
+
             <Route exact path="/reset-password/:id">
               <ResetPassword />
             </Route>
@@ -53,7 +51,6 @@ const RouterURL = withRouter(({ location }) => {
       </Layout>
     </div>
   );
-
 
   const LoginContainer = () => (
     <div>
@@ -80,19 +77,27 @@ const RouterURL = withRouter(({ location }) => {
           <Route exact path="/">
             <PublicContainer />
           </Route>
-        
+
+          <Route exact path="/product-detail/:id">
+            <PublicContainer />
+          </Route>
+
           <Route exact path="/login">
             <LoginContainer />
           </Route>
           <Route exact path="/register">
             <LoginContainer />
           </Route>
-         
+
           <Route exact path="/home">
             <PrivateContainer />
           </Route>
-          
+
           <Route exact path="/reset-password/:id">
+            <PublicContainer />
+          </Route>
+
+          <Route exact path="/product-list/:id">
             <PublicContainer />
           </Route>
         </Switch>
